4bc52d06585a4570537f4943140f277513434471
Ubrillo <72024415+Ubrillo@users.noreply.github.com>
2025-06-07 10:27:35 +0100

    Delete Observer.java

--- Observer.java
+++
@@ -1,9 +0,0 @@
-/**
- * Provides attributes and methods that defines the Obsever
- * @author U. UDOETTE
- * @version 12/24
- */
-package Presentation;
-public interface Observer {
-    public void update();
-}
